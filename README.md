457589efbd694a5ecde36cbf959483caf21faf4e
samAd0san <sohaibsamad.hala@gmail.com>
2024-06-05 22:24:50 +0530

    Navbar Component (responsive)

--- README.md
+++ README.md
@@ -8,3 +8,13 @@
 
 # Add Tailwind
 - <b>assets/styles/globals.css</b> Add the Tailwind CSS directives
+
+# Navbar Component
+- We are Creating the Navbar component (both for desktop view and mobile view)
+
+1. If the user is Logged out just show the 'Login or Registor' Icon and hide the user Add Property, profile drop down and notification button.
+
+2. When the user is Logged in hide the 'Login or Registor' Icon and show 
+the user Add Property, profile drop down and notification button.
+
+- <b>Note:</b> The implementation is done same in the mobile and desktop view.
--- app/layout.jsx
+++ app/layout.jsx
@@ -1,16 +1,19 @@
-import '@/assets/styles/globals.css';
+import Navbar from "@/components/Navbar";
+import "@/assets/styles/globals.css";
 import React from "react";
 
 export const metadata = {
-    title: 'Property Pulse | Best Rental site',
-    description: 'Find your dream rental property'
-}
+  title: "Property Pulse | Best Rental site",
+  description: "Find your dream rental property",
+  keywords: "rental, find rentals, find properties",
+};
 
-const MainLayout = ({children}) => {
+const MainLayout = ({ children }) => {
   return (
     <html lang="en">
       <body>
-        <div>{children}</div>
+        <Navbar />
+        <main>{children}</main>
       </body>
     </html>
   );
