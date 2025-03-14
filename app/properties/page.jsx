d2c1f425ee4f34d97ae4d24e6b7df8c23f60efea
Sohaib samad <sohaibsamad.hala@gmail.com>
2025-03-14 07:31:41 +0530

    Render properties from JSON file

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,11 +1,26 @@
+import PropertyCard from '@/components/PropertyCard';
+import properties from '@/properties.json';
+
 const PropertiesPage = () => {
     return (
-        <div>
-            Properties page
-        </div>
+        <section className='px-4 py-6'>
+            <div className='container-xl lg:container px-4 py-6'>
+                {
+                    properties.length === 0 ? (
+                        <p>Property not Found</p>
+                    ) : (
+                        <div className='grid grid-cols-1 lg:grid-cols-3 gap-6'> 
+                        {
+                            properties.map((property) => (
+                                <PropertyCard key={property._id} property={property} />
+                            ))
+                        }
+                        </div>
+                    )
+                }
+            </div>
+        </section>
     )
 }
 
 export default PropertiesPage;
-
-// localhost:3000/properties
