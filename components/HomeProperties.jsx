54cb6411f597e4c4ac6ab2643a2a8330f9a23fde
Sohaib samad <sohaibsamad.hala@gmail.com>
2025-03-15 23:17:44 +0530

    Partial Changes HomeProperties Component

--- components/HomeProperties.jsx
+++ components/HomeProperties.jsx
@@ -1,6 +1,6 @@
 import properties from '@/properties.json';
-import PropertyCard from './PropertyCard';
 import Link from 'next/link';
+import PropertyCard from './PropertyCard';
 
 const HomeProperties = () => {
     const recentProperties = properties
@@ -8,23 +8,23 @@ const HomeProperties = () => {
         .slice(0, 3);
 
     return (
-        <section className='px-4 py-6'>
-            <div className='container-xl lg:container px-4 py-6'>
-                {
-                    properties.length === 0 ? (
-                        <p>Property not Found</p>
-                    ) : (
-                        <div className='grid grid-cols-1 lg:grid-cols-3 gap-6'>
-                            {
-                                recentProperties.map((property) => (
-                                    <PropertyCard key={property._id} property={property} />
-                                ))
-                            }
-                        </div>
-                    )
-                }
-            </div>
-
+        <>
+            <section className='px-4 py-6'>
+                <div className='container-xl lg:container m-auto'>
+                    <h2 className='text-3xl font-bold text-blue-500 mb-6 text-center'>
+                        Recent Properties
+                    </h2>
+                    <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+                        {recentProperties.length === 0 ? (
+                            <p>No Properties Found</p>
+                        ) : (
+                            recentProperties.map((property) => (
+                                <PropertyCard key={property._id} property={property} />
+                            ))
+                        )}
+                    </div>
+                </div>
+            </section>
             <section className='m-auto max-w-lg my-10 px-6'>
                 <Link
                     href='/properties'
@@ -33,8 +33,8 @@ const HomeProperties = () => {
                     View All Properties
                 </Link>
             </section>
-        </section>
-    )
-}
+        </>
+    );
+};
 
 export default HomeProperties;
