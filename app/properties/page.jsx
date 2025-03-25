dd6cbf4b10e37f9795ca24d094a01b5fafcb6f7d
samAd0san <sohaibsamad.hala@gmail.com>
2025-03-25 18:06:53 +0530

    Added Property & User Models Fetch Data Using Server Component

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,9 +1,11 @@
 import PropertyCard from '@/components/PropertyCard';
-import properties from '@/properties.json';
 import connectDB from '@/config/database';
+import Property from '@/models/Property';
 
 const PropertiesPage = async () => {
     await connectDB();
+    const properties = await Property.find({}).lean();
+
     return (
         <section className='px-4 py-6'>
             <div className='container-xl lg:container px-4 py-6'>
--- components/HomeProperties.jsx
+++ components/HomeProperties.jsx
@@ -1,11 +1,14 @@
-import properties from '@/properties.json';
 import Link from 'next/link';
 import PropertyCard from './PropertyCard';
+import connectDB from '@/config/database';
+import Property from '@/models/Property';
 
-const HomeProperties = () => {
-    const recentProperties = properties
-        .sort(() => Math.random() - Math.random())
-        .slice(0, 3);
+const HomeProperties = async () => {
+    await connectDB();
+    const recentProperties = await Property.find({})
+        .sort({ createdAt: -1})
+        .limit(3)
+        .lean();
 
     return (
         <>
