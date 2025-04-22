9454e74d8abda4f233ccec83c3ebfdaafc79b182
samAd0san <sohaibsamad.hala@gmail.com>
2025-04-22 22:35:50 +0530

    Added SignOut

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -223,6 +223,10 @@ const Navbar = () => {
                       role='menuitem'
                       tabIndex='-1'
                       id='user-menu-item-2'
+                      onClick={() => {
+                        setIsProfileMenuOpen(false);
+                        signOut({ callbackUrl: '/' });
+                      }}
                     >
                       Sign Out
                     </Link>
