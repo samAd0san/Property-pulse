3b566c1991992e0d658035d8c981ee72730161f4
samAd0san <sohaibsamad.hala@gmail.com>
2025-06-08 23:46:19 +0530

    Added Search in PropertyPage

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,30 +1,35 @@
 import PropertyCard from '@/components/PropertyCard';
-import connectDB from '@/config/database';
+import PropertySearchForm from '@/components/PropertySearchForm';
 import Property from '@/models/Property';
+import connectDB from '@/config/database';
 
 const PropertiesPage = async () => {
-    await connectDB();
-    const properties = await Property.find({}).lean();
+  await connectDB();
+  const properties = await Property.find({}).lean();
 
-    return (
-        <section className='px-4 py-6'>
-            <div className='container-xl lg:container px-4 py-6'>
-                {
-                    properties.length === 0 ? (
-                        <p>Property not Found</p>
-                    ) : (
-                        <div className='grid grid-cols-1 lg:grid-cols-3 gap-6'> 
-                        {
-                            properties.map((property) => (
-                                <PropertyCard key={property._id} property={property} />
-                            ))
-                        }
-                        </div>
-                    )
-                }
+  return (
+    <>
+      <section class='bg-blue-700 py-4'>
+        <div class='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-start'>
+          <PropertySearchForm />
+        </div>
+      </section>
+      <section className='px-4 py-6'>
+        <div className='container-xl lg:container m-auto px-4 py-6'>
+          <h1 className='text-2xl mb-4'>Browse Properties</h1>
+          {properties.length === 0 ? (
+            <p>No properties found</p>
+          ) : (
+            <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+              {properties.map((property, index) => (
+                <PropertyCard property={property} key={index} />
+              ))}
             </div>
-        </section>
-    )
-}
+          )}
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default PropertiesPage;
