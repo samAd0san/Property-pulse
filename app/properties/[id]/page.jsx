87a6a8cc573b794ff22a3da9e4d95e3d5df27063
samAd0san <sohaibsamad.hala@gmail.com>
2025-06-08 17:13:14 +0530

    Share buttons

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -4,7 +4,7 @@ import PropertyHeaderImage from "@/components/PropertyHeaderImage";
 import PropertyDetails from "@/components/PropertyDetails";
 import PropertyImages from "@/components/PropertyImages";
 import BookmarkButton from "@/components/BookmarkButton";
-import ShareButtons from "@/components/ShareButton";
+import ShareButtons from "@/components/ShareButtons";
 import PropertyContactForm from "@/components/PropertyContactForm";
 
 const DynamicPropertyId = async ({ params }) => {
