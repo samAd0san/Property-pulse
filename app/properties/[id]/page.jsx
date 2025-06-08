37487bd485853fd7dac859d0dde2fdc5df63b3e3
samAd0san <sohaibsamad.hala@gmail.com>
2025-06-08 23:54:01 +0530

    fixed type parse error

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,48 +1,57 @@
-import connectDB from "@/config/database";
-import Property from "@/models/Property";
-import PropertyHeaderImage from "@/components/PropertyHeaderImage";
-import PropertyDetails from "@/components/PropertyDetails";
-import PropertyImages from "@/components/PropertyImages";
-import BookmarkButton from "@/components/BookmarkButton";
-import ShareButtons from "@/components/ShareButtons";
-import PropertyContactForm from "@/components/PropertyContactForm";
+import PropertyHeaderImage from '@/components/PropertyHeaderImage';
+import PropertyDetails from '@/components/PropertyDetails';
+import connectDB from '@/config/database';
+import Property from '@/models/Property';
+import PropertyImages from '@/components/PropertyImages';
+import BookmarkButton from '@/components/BookmarkButton';
+import ShareButtons from '@/components/ShareButtons';
+import PropertyContactForm from '@/components/PropertyContactForm';
+import { convertToSerializeableObject } from '@/utils/convertToObject';
+import Link from 'next/link';
+import { FaArrowLeft } from 'react-icons/fa';
 
-const DynamicPropertyId = async ({ params }) => {
-    await connectDB();
-    const property = await Property.findById(params.id).lean();
+const PropertyPage = async ({ params }) => {
+  await connectDB();
+  const propertyDoc = await Property.findById(params.id).lean();
+  const property = convertToSerializeableObject(propertyDoc);
 
+  if (!property) {
     return (
-        <>
-            {/* TOP HEADER IMAGE */}
-            <PropertyHeaderImage image={property.images[0]} />
+      <h1 className='text-center text-2xl font-bold mt-10'>
+        Property Not Found
+      </h1>
+    );
+  }
 
-            {/* CONTENT BELOW HEADER IMAGE */}
-            <section className='bg-blue-50'>
-                <div className='container m-auto py-10 px-6'>
-                    <div className='grid grid-cols-1 md:grid-cols-70/30 w-full gap-6'>
-                        {/* LEFT SIDE!! */}
-                        {/* Property Details */}
-                        <PropertyDetails property={property} />
+  return (
+    <>
+      <PropertyHeaderImage image={property.images[0]} />
+      <section>
+        <div className='container m-auto py-6 px-6'>
+          <Link
+            href='/properties'
+            className='text-blue-500 hover:text-blue-600 flex items-center'
+          >
+            <FaArrowLeft className='mr-2' /> Back to Properties
+          </Link>
+        </div>
+      </section>
+      <section className='bg-blue-50'>
+        <div className='container m-auto py-10 px-6'>
+          <div className='grid grid-cols-1 md:grid-cols-70/30 w-full gap-6'>
+            <PropertyDetails property={property} />
 
-                        {/* RIGHT SIDE!! */}
-                        <aside className='space-y-4'>
-
-                            {/* <!-- Sidebar --> */}
-                            {/* Bookmark Button */}
-                            <BookmarkButton property={property} />
-                            
-                            {/* Share Buttons */}
-                            <ShareButtons property={property} />
-
-                            {/* <!-- Contact Form --> */}
-                            <PropertyContactForm property={property} />
-                        </aside>
-                    </div>
-                </div>
-            </section>
-            <PropertyImages images={property.images} />
-        </>
-    )
-}
-
-export default DynamicPropertyId;
+            {/* <!-- Sidebar --> */}
+            <aside className='space-y-4'>
+              <BookmarkButton property={property} />
+              <ShareButtons property={property} />
+              <PropertyContactForm property={property} />
+            </aside>
+          </div>
+        </div>
+      </section>
+      <PropertyImages images={property.images} />
+    </>
+  );
+};
+export default PropertyPage;
